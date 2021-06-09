31fe722daa8d9ffd4fe9667838f49d89286bfd96
aprian1337 <dwikyapriyan@gmail.com>
2021-06-09 22:31:37 +0700

    adding utils

--- app/src/main/java/com/aprian1337/batique/core/utils/DataMapper.kt
+++ app/src/main/java/com/aprian1337/batique/core/utils/DataMapper.kt
@@ -1,4 +1,50 @@
 package com.aprian1337.batique.core.utils
 
-class DataMapper {
+import com.aprian1337.batique.core.data.source.local.entity.BatikEntity
+import com.aprian1337.batique.core.data.source.remote.response.BatikResponse
+import com.aprian1337.batique.core.domain.model.Batik
+
+object DataMapper {
+    fun batikResToEnt(data: BatikResponse): List<BatikEntity> {
+        val batikArrayList = ArrayList<BatikEntity>()
+        data.hasil.map {
+            val batik = BatikEntity(
+                it.id,
+                it.namaBatik,
+                it.daerahBatik,
+                it.maknaBatik,
+                it.hargaRendah,
+                it.hargaTinggi,
+                it.linkBatik,
+                false,
+            )
+            batikArrayList.add(batik)
+        }
+        return batikArrayList
+    }
+
+    fun batikEntToDom(data: List<BatikEntity>): List<Batik> =
+        data.map {
+            Batik(
+                it.batikId,
+                it.namaBatik,
+                it.daerahBatik,
+                it.maknaBatik,
+                it.hargaRendah,
+                it.hargaTinggi,
+                it.imgBatik,
+                it.isFavorite,
+            )
+        }
+
+    fun batikDomToEnt(data: Batik) = BatikEntity(
+        data.idBatik,
+        data.namaBatik,
+        data.daerahBatik,
+        data.maknaBatik,
+        data.hargaRendah,
+        data.hargaTinggi,
+        data.imgBatik,
+        data.isFavorite,
+    )
 }
