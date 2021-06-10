96e38f44546d9f8e5dee0dbecb317f538685f077
aprian1337 <dwikyapriyan@gmail.com>
2021-06-10 12:57:14 +0700

    update splash screen activity

--- app/src/main/java/com/aprian1337/batique/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/com/aprian1337/batique/splashscreen/SplashScreenActivity.kt
@@ -1,11 +1,22 @@
 package com.aprian1337.batique.splashscreen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.aprian1337.batique.MainActivity
+import com.aprian1337.batique.R
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        val timeSplashScreen = 4000L
+        Handler(Looper.getMainLooper()).postDelayed({
+            Intent(this@SplashScreenActivity, MainActivity::class.java).apply {
+                startActivity(this)
+            }
+        }, timeSplashScreen)
     }
 }
